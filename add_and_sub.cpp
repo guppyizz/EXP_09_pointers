ee6ea4278b5778a077e3d30a7ba4874d19d9719f
Updesh arora <updesh.arora4200@gmail.com>
2025-08-13 20:41:08 +0530

    Create add_and_sub.cpp

---
+++ add_and_sub.cpp
@@ -0,0 +1,22 @@
+// UPDESH ARORA
+// PRN - 24070123124
+// ENTC B-3
+// EXPERIMENT 09
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a=4;
+    int b=8;
+    int*a1;
+    a1=&a;
+    int*b1;
+    b1=&b 
+    int sum,diff;
+    sum=*a1+*b1;
+    diff=*a1-*b1;
+    cout<<"The Sum is:- "<<sum<<endl;
+    cout<<"The Differe+nce is:- "<<diff<<endl;
+
+}
